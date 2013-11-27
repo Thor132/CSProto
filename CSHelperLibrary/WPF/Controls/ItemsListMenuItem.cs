26f2620ea71fd53b8ce77aaddbc2bc5117ee50ef
Travis Thornton <travis.thor@gmail.com>
2013-11-27 15:20:25 -0800

    Adds a control that creates MenuItem instances for a bound string list. This can be used for recent documents/items.

---
+++ CSHelperLibrary/WPF/Controls/ItemsListMenuItem.cs
@@ -0,0 +1,197 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ItemsListMenuItem.cs" company="N/A">
+// (C) Travis Thornton 2013
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace CSHelperLibrary.WPF.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// A MenuItem that inserts controlled MenuItems based on a bound string collection.
+    /// </summary>
+    public class ItemsListMenuItem : MenuItem
+    {
+        private MenuItem parentMenu;
+        private MenuItem subMenu;
+        private List<MenuItem> createdMenuItems = new List<MenuItem>();
+
+        public static readonly DependencyProperty ItemListProperty = DependencyProperty.Register("ItemList", typeof(ObservableCollection<string>), typeof(ItemsListMenuItem));
+        public static readonly DependencyProperty ItemCommandProperty = DependencyProperty.Register("ItemCommand", typeof(ICommand), typeof(ItemsListMenuItem));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ItemsListMenuItem"/> class.
+        /// </summary>
+        public ItemsListMenuItem()
+        {
+            this.MaxPathLength = 0;
+            this.Loaded += this.MenuItemList_Loaded;
+            this.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Gets or sets the item list.
+        /// </summary>
+        public ObservableCollection<string> ItemList
+        {
+            get { return this.GetValue(ItemListProperty) as ObservableCollection<string>; }
+            set { this.SetValue(ItemListProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the command for each item.
+        /// The item string will be passed as the parameter to this command.
+        /// </summary>
+        public ICommand ItemCommand
+        {
+            get { return this.GetValue(ItemCommandProperty) as ICommand; }
+            set { this.SetValue(ItemCommandProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the submenu header.
+        /// If this is not set then the items will be in the parent of this control.
+        /// </summary>
+        public string SubMenuHeader { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum path length if the item list is a path.
+        /// Setting this value to 0 or less than 0 will not truncate the path.
+        /// </summary>
+        public int MaxPathLength { get; set; }
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Sets up this control's functionality once it is loaded.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void MenuItemList_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Loaded -= this.MenuItemList_Loaded;
+
+            MenuItem parent = this.Parent as MenuItem;
+            if (parent == null)
+            {
+                throw new Exception("Parent must be a type of MenuItem.");
+            }
+
+            this.parentMenu = parent;
+            this.ItemList.CollectionChanged += this.ItemList_CollectionChanged;
+
+            if (!string.IsNullOrEmpty(this.SubMenuHeader))
+            {
+                this.subMenu = new MenuItem() { Header = this.SubMenuHeader };
+                this.parentMenu.Items.Insert(this.parentMenu.Items.IndexOf(this) + 1, this.subMenu);
+            }
+
+            this.UpdateMenu();
+        }
+
+        /// <summary>
+        /// Updates the menu when the item list is changed.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void ItemList_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            this.UpdateMenu();
+        }
+
+        #endregion
+
+        #region MenuItem Update Functionality
+
+        /// <summary>
+        /// Updates the menu.
+        /// If a sub menu name was provided it will enable or disable it depending on if there are items in the list.
+        /// </summary>
+        private void UpdateMenu()
+        {
+            if (this.subMenu != null)
+            {
+                if (this.ItemList.Any())
+                {
+                    if (!this.subMenu.IsEnabled)
+                    {
+                        this.subMenu.IsEnabled = true;
+                    }
+                }
+                else
+                {
+                    if (this.subMenu.IsEnabled)
+                    {
+                        this.subMenu.IsEnabled = false;
+                    }
+                }
+            }
+
+            this.UpdateList();
+        }
+
+        /// <summary>
+        /// Updates the parent MenuItem with the items list.
+        /// TODO: This is clearing and recreating the list each update. Perhaps make it reuse the already created data.
+        /// </summary>
+        private void UpdateList()
+        {
+            MenuItem parent = this.subMenu ?? this.parentMenu;
+            foreach (MenuItem item in createdMenuItems)
+            {
+                parent.Items.Remove(item);
+            }
+
+            this.createdMenuItems.Clear();
+
+            int nextItemIndex = this.subMenu == null ? this.parentMenu.Items.IndexOf(this) + 1 : 0;
+            if (this.ItemList != null && this.ItemList.Any())
+            {
+                int currentIndex = nextItemIndex;
+                foreach (var item in this.ItemList)
+                {
+                    MenuItem newItem = new MenuItem()
+                        {
+                            Header = this.MaxPathLength > 0 ? this.GetTruncatedPath(item) : item,
+                            Command = this.ItemCommand,
+                            CommandParameter = item
+                        };
+
+                    parent.Items.Insert(currentIndex++, newItem);
+                    this.createdMenuItems.Add(newItem);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Path Truncation Functionality
+
+        [DllImport("shlwapi.dll", CharSet = CharSet.Auto)]
+        private static extern bool PathCompactPathEx([Out] StringBuilder pszOut, string szPath, int cchMax, int dwFlags);
+
+        /// <summary>
+        /// Returns the truncated path from the native PathCompactPathEx
+        /// </summary>
+        /// <param name="path">Path to truncate.</param>
+        /// <returns>Truncated path.</returns>
+        private string GetTruncatedPath(string path)
+        {
+            StringBuilder sb = new StringBuilder();
+            PathCompactPathEx(sb, path, this.MaxPathLength, 0);
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ WPFMRUMenuItemTest/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using CSHelperLibrary.WPF;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WPFMRUMenuItemTest
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.MenuItems = new ObservableCollection<string>();
+            for (int i = 0; i < 0; ++i)
+            {
+                this.MenuItems.Add(string.Format("{0} - {1}", i, DateTime.Now));
+            }
+        }
+
+        public ObservableCollection<string> MenuItems { get; set; }
+
+        public ICommand AddCommand
+        {
+            get
+            {
+                return new RelayCommand(param => this.MenuItems.Add(string.Format("{0} - {1}", this.MenuItems.Count, DateTime.Now)), param => { return true; });
+            }
+        }
+
+        public ICommand RemoveCommand
+        {
+            get
+            {
+                return new RelayCommand(param => this.MenuItems.RemoveAt(0), param => this.MenuItems.Any());
+            }
+        }
+
+        public ICommand RecentItemCommand
+        {
+            get
+            {
+                return new RelayCommand(param => MessageBox.Show(string.Format("RecentItemCommand param: {0}", param)));
+            }
+        }
+    }
+}
