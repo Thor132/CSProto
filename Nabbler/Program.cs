8d980de06d7fe1aab77b6b4f26b5efcc6f5ea600
Travis Thornton <travis.thor@gmail.com>
2014-01-16 23:16:49 -0800

    Nabbler overhaul - using BitBlt now. Cleanup.

--- Nabbler/Program.cs
+++ Nabbler/Program.cs
@@ -10,11 +10,12 @@ namespace Nabbler
     using System.IO;
     using System.Linq;
     using System.Runtime.InteropServices;
+    using System.Threading;
     using CommandLine;
     using CommandLine.Text;
 
     [StructLayout(LayoutKind.Sequential)]
-    public struct Rect
+    internal struct Rect
     {
         public int left;
         public int top;
@@ -23,6 +24,42 @@ namespace Nabbler
 
         public int Width { get { return right - left; } }
         public int Height { get { return bottom - top; } }
+    }
+
+    internal static class Native
+    {
+        internal enum ShowWindowCommand : int { SW_HIDE = 0, SW_MINIMIZE = 6, SW_RESTORE = 9 }
+        internal enum RasterOperation : uint { SRC_COPY = 0x00CC0020 }
+        internal enum KeyCodes : int { VK_LMENU = 0xA4 }
+        [Flags]
+        internal enum KeyboardFlags : int { KEYEVENTF_EXTENDEDKEY = 0x1, KEYEVENTF_KEYUP = 0x2 }
+
+        [DllImport("user32.dll")]
+        internal static extern bool GetClientRect(IntPtr hWnd, out Rect lpRect);
+        [DllImport("user32.dll")]
+        internal static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("user32.dll")]
+        internal static extern int ReleaseDC(IntPtr hwnd, IntPtr hdc);
+        [DllImport("gdi32.dll")]
+        internal static extern int BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hdcSrc, int nXSrc, int nYSrc, RasterOperation rasterOperation);
+
+        [DllImport("user32.dll")]
+        internal static extern bool IsIconic(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        internal static extern IntPtr ShowWindow(IntPtr hWnd, ShowWindowCommand nCmdShow);
+        [DllImport("user32.dll")]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+        [DllImport("user32.dll")]
+        internal static extern int SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        internal static extern bool AllowSetForegroundWindow(int dwProcessId);
+        [DllImport("user32.dll")]
+        internal static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+        [DllImport("user32.dll")]
+        internal static extern void keybd_event(byte bVk, byte bScan, KeyboardFlags dwFlags, int dwExtraInfo);
     }
 
     public class Options
@@ -45,6 +82,8 @@ namespace Nabbler
 
     public class Program
     {
+        private const int RestoreWaitTime = 500;
+
         static void Main(string[] args)
         {
             var options = new Options();
@@ -64,63 +103,87 @@ namespace Nabbler
 
                 foreach (Process process in processes)
                 {
-                    Console.WriteLine("Capturing {0} ID {1}", process.ProcessName, process.Id);
-                    Bitmap bmp = PrintWindow(process.MainWindowHandle);
-                    string outputPath = Path.Combine(options.OutputDirectory, string.Format("{0}-{1}.png", process.ProcessName, process.Id));
-                    bmp.Save(outputPath, ImageFormat.Png);
-                    Console.WriteLine("Saved to {0}", outputPath);
+                    try
+                    {
+                        Console.WriteLine("Capturing {0} ID {1}", process.ProcessName, process.Id);
+                        Bitmap bmp = CaptureProcessMainWindow(process);
+                        if (bmp == null)
+                        {
+                            Console.WriteLine("An error occurred when attempting to capture screenshot.");
+                            continue;
+                        }
+
+                        string outputPath = Path.Combine(options.OutputDirectory, string.Format("{0}-{1}.png", process.ProcessName, process.Id));
+                        bmp.Save(outputPath, ImageFormat.Png);
+                        Console.WriteLine("Saved to {0}", outputPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
                 }
             }
         }
 
-        [DllImport("user32.dll")]
-        public static extern bool GetClientRect(IntPtr hWnd, out Rect lpRect);
-        [DllImport("user32.dll")]
-        public static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
-        [DllImport("user32.dll")]
-        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
-        [DllImport("user32.dll")]
-        private static extern IntPtr ShowWindow(IntPtr hWnd, int nCmdShow);
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-        private const int GWL_STYLE = -16;
-        private const int SW_HIDE = 0;
-        private const int SW_RESTORE = 9;
-        private const int SW_MINIMIZE = 6;
-        private const uint WS_MINIMIZE = 0x20000000;
-
-        public static Bitmap PrintWindow(IntPtr hwnd)
+        public static Bitmap CaptureProcessMainWindow(Process process)
         {
-            bool wasMinimized = false;
-            int style = GetWindowLong(hwnd, GWL_STYLE);
-            if ((style & WS_MINIMIZE) == WS_MINIMIZE)
-            {
-                // Needed to add an explicit hide due to not working properly with the steam window.
-                ShowWindow(hwnd, SW_HIDE);
-                ShowWindow(hwnd, SW_RESTORE);
-                wasMinimized = true;
-            }
+            IntPtr hwnd = process.MainWindowHandle;
+
+            RestoreWindow(hwnd);
+            SetForgroundWindowMethod1(process);
 
             Rect rect;
-            GetWindowRect(hwnd, out rect);
-
-            // http://stackoverflow.com/questions/891345/get-a-screenshot-of-a-specific-application
-            Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
-            Graphics gfxBmp = Graphics.FromImage(bmp);
-            IntPtr hdcBitmap = gfxBmp.GetHdc();
-
-            PrintWindow(hwnd, hdcBitmap, 0);
-
-            gfxBmp.ReleaseHdc(hdcBitmap);
-            gfxBmp.Dispose();
-
-            if (wasMinimized)
+            Native.GetClientRect(hwnd, out rect);
+            if (rect.Width == 0 || rect.Height == 0)
             {
-                ShowWindow(hwnd, SW_MINIMIZE);
+                return null;
             }
 
-            return bmp;
+            IntPtr hdc = Native.GetDC(hwnd);
+            Bitmap bitmap = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                IntPtr dstHdc = graphics.GetHdc();
+                Native.BitBlt(dstHdc, 0, 0, rect.Width, rect.Height, hdc, 0, 0, Native.RasterOperation.SRC_COPY);
+                graphics.ReleaseHdc(dstHdc);
+            }
+
+            Native.ReleaseDC(hwnd, hdc);
+            return bitmap;
+        }
+
+        public static void RestoreWindow(IntPtr hwnd)
+        {
+            if (Native.IsIconic(hwnd))
+            {
+                Native.ShowWindow(hwnd, Native.ShowWindowCommand.SW_RESTORE);
+                Thread.Sleep(RestoreWaitTime);
+            }
+        }
+
+        public static void RepositionWindow(IntPtr hwnd)
+        {
+            Rect winRect;
+            Native.GetWindowRect(hwnd, out winRect);
+            Native.MoveWindow(hwnd, 0, 0, winRect.Width, winRect.Height, true);
+        }
+
+        public static void SetForgroundWindowMethod1(Process process)
+        {
+            Native.AllowSetForegroundWindow(process.Id);
+            Native.SetForegroundWindow(process.MainWindowHandle);
+        }
+
+        public static void SetForgroundWindowMethod2(Process process)
+        {
+            Native.SwitchToThisWindow(process.MainWindowHandle, true);
+        }
+
+        public static void SetForgroundWindowMethod3(Process process)
+        {
+            Native.keybd_event((byte)Native.KeyCodes.VK_LMENU, 0x45, Native.KeyboardFlags.KEYEVENTF_EXTENDEDKEY | 0, 0);
+            Native.keybd_event((byte)Native.KeyCodes.VK_LMENU, 0x45, Native.KeyboardFlags.KEYEVENTF_EXTENDEDKEY | Native.KeyboardFlags.KEYEVENTF_KEYUP, 0);
+            Native.SetForegroundWindow(process.MainWindowHandle);
         }
     }
 }
